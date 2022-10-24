f2d80f9ff5e314a7710bcc483e71cc0b0a097652
santoshtiwari41 <santoshhh066@gmail.com>
2022-10-24 07:41:16 +0545

    initial commit

---
+++ callbackHell.js
@@ -0,0 +1,46 @@
+function register(callback) {
+  setTimeout(() => {
+    console.log("register  end");
+    callback();
+  }, 1000);
+}
+function sendEmail(callback) {
+  setTimeout(() => {
+    console.log("Email send");
+    callback();
+  }, 4000);
+}
+function login(callback) {
+  setTimeout(() => {
+    console.log("login end");
+    callback();
+  }, 5000);
+}
+function getuserData(callback) {
+  setTimeout(() => {
+    console.log("get user data");
+    callback();
+
+  }, 6000);
+}
+function displayuserData() {
+  setTimeout(() => {
+    console.log("user data displayed");
+  }, 1000);
+}
+register(() => 
+{
+  sendEmail(()=>
+  {
+      login(()=>
+      {
+          getuserData(()=>
+          {
+              displayuserData();
+          });
+     
+      });
+      
+  });
+});
+console.log("other application work");
